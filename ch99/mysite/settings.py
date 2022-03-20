12f39780e19b2f5cdd8125abe4a2f41e25b3b534
Juyeon0526 <89025079+Juyeon0526@users.noreply.github.com>
2022-03-20 14:45:12 +0900

    Update settings.py
    secret key 안보이게 수정

--- ch99/mysite/settings.py
+++ ch99/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l#gb!bmr5y%hxij*al9tb++0yt279%3t1xexid76oexpj)5ya7'
+SECRET_KEY = '*******'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
